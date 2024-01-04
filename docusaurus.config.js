89449834b20e555f1a56bbf36219564588974c77
aquarius-wing <aquarius_wing@icloud.com>
2024-01-04 18:18:22 +0800

    社交配图更新

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -73,7 +73,8 @@ const config = {
     /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
     ({
       // Replace with your project's social card
-      image: 'img/docusaurus-social-card.jpg',
+      image: 'img/social-card.jpg',
+      title: 'Meogic Wiki',
       navbar: {
         title: 'Meogic Wiki',
         logo: {
